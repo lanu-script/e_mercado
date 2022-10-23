2a68c7b53c48c9366860f5f55571c7f582960967
Lucia Rey <110506334+lanu-script@users.noreply.github.com>
2022-10-23 18:57:56 -0300

    Comentarios en carrito

--- js/cart.js
+++ js/cart.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", async function() {
     const data = await getJSONData(CART_INFO_URL + "25801.json");
     for(let article of data.data.articles) {
         let row = document.createElement("tr");
+        // crear fila
         let img = document.createElement("td");
         let imgElem = document.createElement("img");
         imgElem.classList.add("w-100");
@@ -15,12 +16,15 @@ document.addEventListener("DOMContentLoaded", async function() {
         img.classList.add("w-25");
         img.appendChild(imgElem);
         row.appendChild(img);
+        // crear imagen
         let name = document.createElement("td");
         name.innerText = article.name;
         row.appendChild(name);
+        // crear nombre
         let cost = document.createElement("td");
         cost.innerText = article.currency + " " + article.unitCost;
         row.appendChild(cost);
+        // crear costo
         let count = document.createElement("td");
         let countInput = document.createElement("input");
         countInput.type = "number";
@@ -29,10 +33,13 @@ document.addEventListener("DOMContentLoaded", async function() {
         countInput.classList.add("form-control");
         count.appendChild(countInput);
         row.appendChild(count);
+        // crear input de unidades
         let subtotal = document.createElement("td");
         calculateSubtotal(subtotal, article, countInput);
         row.appendChild(subtotal);
+        // crear y calcular subtotal
         countInput.addEventListener("click", () => calculateSubtotal(subtotal, article, countInput));
+        // agregar evento para recalcular subtotal cuando cambia conteo
         document.getElementById("article-table").appendChild(row);
     }
 });
